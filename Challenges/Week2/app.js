2885bd54e4386c8fa00346166892c2239debaada
Blak3Nick <blaken2021@gmail.com>
2016-09-02 12:50:59 -0400

    wrote the logic and changed some styles

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,13 +1,30 @@
 //variables
 var user_Input= '';
 
-
 //create function
   //control and return statements
+window.onload = function() {
 
 //Prompt user for input and store variable
+user_Input = prompt("What is the customary term when greeting someone?");
 
+if (user_Input === 'Hello') {
+    alert('Hello world!');
+}
+else {
+  alert('Does not compute...');
+}
+document.getElementById("trybtn").addEventListener("click", function(){
+    user_Input = prompt("What is the customary term when greeting someone?");
+
+if (user_Input === 'Hello') {
+    alert('Hello world!');
+}
+else {
+  alert('Does not compute...');
+}
+});
 //Call function
-
+}
 //Alert user results
-console.log('test');
+
