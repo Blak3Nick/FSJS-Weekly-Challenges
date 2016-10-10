63288cdde47f2fa7ddbbc1d2b95a2920f82671c5
Blak3Nick <blaken2021@gmail.com>
2016-10-10 10:24:08 -0400

    working with the returned data

--- Challenges/Week7/github.js
+++ Challenges/Week7/github.js
@@ -1,23 +1,25 @@
 const https = require('https');
 var username = 'Blak3Nick';
-
+var exports = module.exports = {};
 var options = {
   hostname: 'api.github.com',
   port: 443,
-  path: '/search/users?q=',
+  path: '/users/',
   method: 'GET',
   headers: { 'User-Agent': 'Blak3Nick' }
 };
-username = 'bpleazy';
+
+
+
+exports.getRepos = function(username){
 options.path += username;
-
-
+options.path += '/repos';
 var req = https.request(options, (res) => {
   console.log('statusCode:', res.statusCode);
   console.log('headers:', res.headers);
 
   res.on('data', (d) => {
-    process.stdout.write(d);
+    process.stdout.write(d)[0][0].name;
   });
 });
 req.end();
@@ -25,3 +27,4 @@ req.end();
 req.on('error', (e) => {
   console.error(e);
 });
+}
