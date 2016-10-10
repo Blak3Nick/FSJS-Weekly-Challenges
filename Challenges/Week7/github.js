2b6b22ddcf1bf78acc04586b820ee487b80e9e17
Blak3Nick <blaken2021@gmail.com>
2016-10-10 09:25:13 -0400

    working with the data

--- Challenges/Week7/github.js
+++ Challenges/Week7/github.js
@@ -1,67 +1,27 @@
-var https = require('https');
-
+const https = require('https');
 var username = 'Blak3Nick';
 
-function getRepos(username, repos){
-  var message = username + ' has ' +repos +' public ' ;
-  console.log(message);
-}
+var options = {
+  hostname: 'api.github.com',
+  port: 443,
+  path: '/search/users?q=',
+  method: 'GET',
+  headers: { 'User-Agent': 'Blak3Nick' }
+};
+username = 'bpleazy';
+options.path += username;
 
 
-var req = https.get('https://api.github.com/search/users?q=' + username + ".json", function(response){
-  console.log(response.statusCode);
+var req = https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
 });
+req.end();
 
-//See readme for challenge instructions
-
-/*
- * Note about github api: requires User-Agent header to be set. This can be done
- * in Node by passing an options object (rather than a simple url string) as the
- * first param to the https.get() function
- *
- * So something like
- * */
-// var http = require('http');
-//
-// var options = {
-//   hostname: 'https://api.github.com/search/users?q=Blak3Nick'
-//   //,port: app.get('port')
-//   ,path: '/users'
-//   ,method: 'GET'
-//   ,headers: { 'Content-Type': 'application/json' }
-// };
-//
-// var req = http.request(options, function(res) {
-//   res.setEncoding('utf8');
-//   res.on('data', function (data) {
-//     console.log(data); // I can't parse it because, it's a string. why?
-//   });
-// });
-// req.on('error', function(e) {
-//   console.log('problem with request: ' + e.message);
-// });
-// req.end();
-
-
- // var options = {
- //    hostname: "api.github.com",
- //    path: '',
- //    headers: {
- //      'User-Agent': 'Blak3Nick'
- //    }
- //  };
-
-
-// function getRepos(username){
-//   api = "https://api.github.com/search/users?q=";
-//       api+= username;
-//   $.getJSON( api, function( data ) {
-//     var items = [];
-//     $.each( data, function( key, val ) {
-//       items.push(  key + "'>" + val  );
-//     });
-//
-//     console.log(items);
-//   });
-// }
-// getRepos("Blak3Nick");
+req.on('error', (e) => {
+  console.error(e);
+});
